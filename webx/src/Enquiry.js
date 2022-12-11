825ebaac2438d1ad27579a64ccd6eca6819b7265
akash kadbhane <113015585+akashkadbhane009@users.noreply.github.com>
2022-12-11 11:19:43 +0530

    web x
    updates;
    1. nav bar
    2. validation to all login and signups
    3.footer modified
    
    Co-Authored-By: VibhaSaroj-JH <113436612+VibhaSaroj-JH@users.noreply.github.com>
    Co-Authored-By: Basant Chaudashi <113439469+Basantchaudashi@users.noreply.github.com>

---
+++ webx/src/Enquiry.js
@@ -0,0 +1,126 @@
+// var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+// document.forms.[form name].(namw attr val).value
+function validEnq() {
+    var name =
+        document.forms.RegForm.Name.value;
+    var email =
+        document.forms.RegForm.EMail.value;
+    var phone =
+        document.forms.RegForm.Telephone.value;
+    var what =
+        document.forms.RegForm.Subject.value;
+    var password =
+        document.forms.RegForm.Password.value;
+    var rePassword =
+        document.forms.RegForm.rePassword.value;
+
+    var address =
+        document.forms.RegForm.Address.value;
+    var regEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;  //Javascript reGex for Email Validation.
+    var regPhone = /^\d{10}$/;                                        // Javascript reGex for Phone Number validation.
+    var regName = /\d+$/g;                                    // Javascript reGex for Name validation
+
+    if (name == "" || regName.test(name)) {
+        window.alert("Please enter your name properly.");
+        // name.focus();
+        return false;
+    }
+
+    if (address == "") {
+        window.alert("Please enter your address.");
+        address.focus();
+        return false;
+    }
+
+    if (email == "" || !regEmail.test(email) || regName.test(email)) {
+        window.alert("Please enter a valid e-mail address.");
+        email.focus();
+        return false;
+    }
+
+    if (password == "") {
+        alert("Please enter your password");
+        password.focus();
+        return false;
+    }
+
+    if (password.length < 6) {
+        alert("Password should be atleast 6 character long");
+        password.focus();
+        return false;
+
+    }
+
+    if (password == rePassword) {
+        alert("Password does not match!");
+        password.focus();
+        rePassword.focus();
+        return false;
+
+    }
+    if (phone == "" || !regPhone.test(phone)) {
+        alert("Please enter valid phone number.");
+        phone.focus();
+        return false;
+    }
+    return true;
+}
+//form validation for username?
+//form validation for password js?
+// function validateForm(){
+//     var pass1 = document.getElementsByName("password")[0].value;
+//     var pass2 = document.getElementsByName("password2")[0].value;
+//     if (pass1 === pass2) {
+//         alert("Passwords Match");
+//     } else {
+//         alert("Passwords Do Not Match");
+//     }
+// }
+
+// Enter First and Last Name
+// <input type = "text" id = "name" /><br/>
+// Enter Your Email Address
+// <input type = "text" id = "email" /><br/>
+// Please Enter a Username
+// <input type = "text" id = "username" /><br/>
+// Please Enter a Password
+// <input type = "password" name = "password" /><br/>
+// Enter Your Password Again
+// <input type = "Password" name= "password2" /><br/>
+
+// <button type = "button" id = "validate" onclick = "validateForm();">Validate Password</button>
+// <button type = "button" id = "create" onclick = "submit()">Create Profile</button>
+
+
+
+// https://sheetdb.io/api/v1/wkeqqj3ej1gn6
+
+
+
+async function Submit() {
+    var email = document.forms.Eform.EMail.value;
+    var PNO = document.forms.Eform.PNo.value;
+    var lang = document.forms.Eform.lang.value;
+    var web = document.forms.Eform.web.value;
+    var msg = document.forms.Eform.msg.value;
+    console.log(email);
+    console.log(PNO);
+    console.log(lang);
+    console.log(web);
+    console.log(msg);
+
+    fetch("https://api.apispreadsheets.com/data/PjaXHBpIp7zZAcUm/", {
+        method: "POST",
+        body: JSON.stringify({"data": {"EMail":email,"PNO":PNO,"lang":lang,"web":web,"msg":msg}}),
+    }).then(res =>{
+        if (res.status === 200){
+            // SUCCESS
+            alert(res.status+res.statusText)
+        }
+        else{
+            // ERROR
+            alert(res.status+res.statusText)
+        }
+    })
+
+}
---
+++ webx/src/login.js
@@ -0,0 +1,32 @@
+// var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+
+function validLogin() {
+    
+    var email =
+        document.forms.RegForm.EMail.value;
+
+    var password =
+        document.forms.RegForm.Password.value;
+    var regEmail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;  //Javascript reGex for Email Validation.
+
+    if (email == "" || !regEmail.test(email)) {
+        window.alert("Please enter a valid e-mail address.");
+        email.focus();
+        return false;
+    }
+      
+    if (password == "") {
+        alert("Please enter your password");
+        password.focus();
+        return false;
+    }
+
+    if(password.length <6){
+        alert("Password should be atleast 6 character long");
+        password.focus();
+        return false;
+
+    }
+
+    return true;
+}
